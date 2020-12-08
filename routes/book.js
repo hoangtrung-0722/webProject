d1fe6725c6753ca347cbb06bffbced8b46e1a392
hoangtrung-0722 <hoangtrung.220720@gmail.com>
2020-12-08 16:51:52 +0700

    fix link to book's detail

--- routes/book.js
+++ routes/book.js
@@ -5,6 +5,8 @@ const bookController = require('../controllers/bookController');
 
 router.get('/', bookController.index);
 
+router.get('/books', bookController.index);
+
 router.get('/book_detail/:id', bookController.detail);
 
 module.exports = router;
